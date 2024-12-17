23bf7c90b535d2c369d1faaef784dcd6c9fd36c7
AyxanHesenzade <ayxanhesen1234@gmail.com>
2024-12-17 12:16:22 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Vortex Vivid Innovations səhifəsi yükləndi.");
+    // Form doğrulama və ya digər funksiyalar buraya əlavə edilə bilər.
+});
